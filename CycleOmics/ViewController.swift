9ab2b1cbcb7ddc54e0fe0ba515d32a2ab55493a8
Mojtaba Koosej <mkoosej@gmail.com>
2016-05-29 14:45:52 -0700

    Initial Commit

---
+++ CycleOmics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CycleOmics
+//
+//  Created by Mojtaba Koosej on 5/29/16.
+//  Copyright © 2016 Curio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
