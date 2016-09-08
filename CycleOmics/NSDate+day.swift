ea199b83a37152092e17ced3bd490bb918cf654a
Mojtaba Koosej <mkoosej@gmail.com>
2016-09-07 17:16:22 -0700

    moved the date logics from ProfileVS to NSDate category

--- CycleOmics/NSDate+day.swift
+++ CycleOmics/NSDate+day.swift
@@ -28,5 +28,44 @@ extension NSDate {
         return NSDateComponents(date: endOfDay!, calendar: NSCalendar.currentCalendar())
     }
     
+    class func daysInThisWeek() -> ([NSDate], todayIndex:Int) {
+        // create calendar
+        let calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)!
+        
+        // today's date
+        let today = NSDate()
+        let todayComponent = calendar.components([.Day, .Month, .Year], fromDate: today)
+        
+        // range of dates in this week
+        let thisWeekDateRange = calendar.rangeOfUnit(.Day, inUnit:.WeekOfMonth, forDate:today)
+        
+        // date interval from today to beginning of week
+        let dayInterval = thisWeekDateRange.location - todayComponent.day
+        
+        // date for beginning day of this week, ie. this week's Sunday's date
+        let beginningOfWeek = calendar.dateByAddingUnit(.Day, value: dayInterval, toDate: today, options: .MatchNextTime)
+        
+        var dates: [NSDate] = []
+        
+        // to include days of the week belongs to past month
+        // we should always have 7 days
+        for i in (thisWeekDateRange.length-7) ..< thisWeekDateRange.length {
+            let date = calendar.dateByAddingUnit(.Day, value: i, toDate: beginningOfWeek!, options: .MatchNextTime)!
+            dates.append(date)
+        }
+        
+        let todayIndex = -(dayInterval + (thisWeekDateRange.length-7))
+        
+        return (dates,todayIndex)
+    }
+    
+    var getLocalizedDayofWeek:String {
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let dayOfWeekString = dateFormatter.stringFromDate(self)
+        
+        return dayOfWeekString
+    }
     
 }
